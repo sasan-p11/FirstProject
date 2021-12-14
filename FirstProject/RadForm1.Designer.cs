0e126c6d583c3805dc1184715fa681b17868b192
sasanfarsani11@gmail.com <zohre.jafary76@gmail.com>
2021-12-14 14:47:39 +0330

    addDataGruid

--- FirstProject/RadForm1.Designer.cs
+++ FirstProject/RadForm1.Designer.cs
@@ -29,33 +29,100 @@ namespace FirstProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.radButton1 = new Telerik.WinControls.UI.RadButton();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.UserId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Family = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RegisterDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
-            // radButton1
+            // dataGridView2
             // 
-            this.radButton1.Location = new System.Drawing.Point(105, 75);
-            this.radButton1.Name = "radButton1";
-            this.radButton1.Size = new System.Drawing.Size(131, 46);
-            this.radButton1.TabIndex = 1;
-            this.radButton1.Text = "radButton1";
-            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserId,
+            this.Gender,
+            this.FirstName,
+            this.Family,
+            this.Age,
+            this.RegisterDate,
+            this.UserName,
+            this.Password});
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(846, 150);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // UserId
+            // 
+            this.UserId.HeaderText = "UserId";
+            this.UserId.Name = "UserId";
+            // 
+            // Gender
+            // 
+            this.Gender.HeaderText = "Gender";
+            this.Gender.Name = "Gender";
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.Name = "FirstName";
+            // 
+            // Family
+            // 
+            this.Family.HeaderText = "Family";
+            this.Family.Name = "Family";
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Age";
+            this.Age.Name = "Age";
+            // 
+            // RegisterDate
+            // 
+            this.RegisterDate.HeaderText = "RegisterDate";
+            this.RegisterDate.Name = "RegisterDate";
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(12, 221);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(130, 34);
+            this.radButton2.TabIndex = 1;
+            this.radButton2.Text = "Log";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // UserName
+            // 
+            this.UserName.HeaderText = "UserName";
+            this.UserName.Name = "UserName";
+            // 
+            // Password
+            // 
+            this.Password.HeaderText = "Password";
+            this.Password.Name = "Password";
             // 
             // RadForm1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 158);
-            this.Controls.Add(this.radButton1);
+            this.ClientSize = new System.Drawing.Size(851, 267);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.dataGridView2);
             this.Name = "RadForm1";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
-            this.Text = "RadForm1";
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
@@ -63,5 +130,16 @@ namespace FirstProject
 
         #endregion
         private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Family;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RegisterDate;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Password;
     }
 }
--- FirstProject/RadForm1.cs
+++ FirstProject/RadForm1.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -23,12 +25,27 @@ namespace FirstProject
         {
             InitializeComponent();
         }
-        private void radButton1_Click(object sender, EventArgs e)
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            PracticeEntities entities = new PracticeEntities();
+            var users = entities.User.Select(c => c).ToList();
+            var loginUser = entities.Login.Select(c => c).ToList();
+            
+            DataGridViewRow row = new DataGridViewRow();
+            for (int i = 0; i < users.Count ; i++)
+            {
+                dataGridView2.Rows.Add(users[i].UserId, users[i].Gender, users[i].FirstName, users[i].Family, users[i].Age, 
+                                                users[i].RegisterDate,loginUser[i].UserName, loginUser[i].Password);
+            }
+
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
         {
             XmlDocument log4netConfig = new XmlDocument();
             log4netConfig.Load(File.OpenRead("log4net.config"));
             var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
-            log4net.Config.XmlConfigurator.Configure(repo,log4netConfig["log4net"]);
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
 
             log.Info("Hello logging world!");
             log.Error("Hello logging world!");
